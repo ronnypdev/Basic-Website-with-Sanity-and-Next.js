cda8f4cfc00e379c052e6749cf2ac2c26a31fb65
Ronny Perez <ronnypdev@gmail.com>
2025-01-21 12:53:36 -0500

    Implmented Event, Artist, and evnue type content schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,5 @@
-import { eventType } from "./eventType"
+import {eventType} from './eventType'
+import {artistType} from './artistType'
+import {venueType} from './venueType'
 
-export const schemaTypes = [
-  eventType
-]
+export const schemaTypes = [artistType, eventType, venueType]
